24105da68461b2e7faeebf9e7963172d3c612b70
thoriqulhaq <thoriqulhaqjibril1@gmail.com>
2021-11-04 22:48:35 +0700

    Initialize View, Controller, Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CommunityController;
+use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CommunityController::class, 'viewLandingPage']);
+
+Route::get('/admin', [StaffController::class, 'viewLandingPage']);
