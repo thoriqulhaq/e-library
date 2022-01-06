b6b16bc5ac3365facfdde2456ffcebb333dbb6cf
Hennoshin <m.irfan.hilmi@gmail.com>
2022-01-06 10:31:28 +0700

    Add relation to public users table

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\PublicUser;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
@@ -42,10 +43,14 @@ class RegisteredUserController extends Controller
 
         $user = User::create([
             'name' => $request->name,
-            'nim' => $request->nim,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $dt = new PublicUser();
+        $dt->id_number = $request->nim;
+
+        $user->details()->save($dt);
 
         event(new Registered($user));
 
