31c242e6a1f17763024e6fc04efe0c5567301b86
Hennoshin <m.irfan.hilmi@gmail.com>
2022-01-06 10:53:37 +0700

    Improve client side interaction

--- resources/views/staff/editbook.blade.php
+++ resources/views/staff/editbook.blade.php
@@ -4,12 +4,35 @@
 
 
     <script>
+      
+      $(document).ready(function () {
+
+        // Attach keyup event on Book Description to update character count
+        $("textarea[name='description']").keyup(function (event) {
+          let chars = $(this).next();
+          chars.html($(this).val().length + "/500");
+        });
+
+        window.addEventListener("beforeunload", function (event) {
+          event.preventDefault();
+          let confirmMessage = "\o/";
+
+          return confirmMessage;
+        });
+      });
+
+      // Function to add more Author input fields
+      // Limit to 5 input fields
+      // First input field changed to delete after 5 fields
+      // Changed back to add when deletion of one input field
       var authorValue = [];
       var authorCount = 1;
       function addAuthor() {
-        var value = document.getElementById("author").value
+        var value = document.getElementById("authors").value
 
-        $("#authors").children().eq(-1).before(
+        // Find the last row (book description) and add input field before it
+        let currentElement = $("#authors").children();
+        currentElement.eq(-1).before(
         `<div class="col-md-12 mt-2 mb-3">
               <div class="input-group input-group-sm">
                 <input class="form-control" type="text" name="author[]" required value="` + value + `"/>
@@ -20,11 +43,40 @@
             authorCount++;
             authorValue.push(value);
             console.log(authorValue)
-            document.getElementById("author").value = "";
+            document.getElementById("authors").value = "";
+
+            // Change the first input field add button to delete, limit 5 authors
+            if (authorCount >= 5) {
+              currentElement.eq(0).find("i")
+              .attr("onclick", "deleteAuthor(this)")
+              .css("color", "red")
+              .html("delete");
+
+            }
       }
 
+      // Delete the Author input field
       function deleteAuthor(el) {
-        $(el).parent().parent().remove();
+        let currentElement = $(el).parent().parent();
+
+        // First input field has "label" tag, if will be deleted, put new label on second field before deletion
+        if (currentElement.has("label").length) {
+          currentElement.next().removeClass("mt-2")
+          .prepend("<label class='form-label'>Author</label>");
+        }
+
+        currentElement.remove();
+
+        // Change back the first Author input field to add if 5 authors limit is reached
+        currentElement = $("#authors").children().eq(0);
+        if (authorCount >= 5) {
+          currentElement.find("i")
+          .attr("onclick", "addAuthor()")
+          .css("color", "#008000")
+          .html("add_box");
+        }
+
+        authorCount--;
       }
     </script>
 
@@ -82,7 +134,7 @@
               <label class="form-label">Author</label>
               @endif
               <div class="input-group input-group-sm">
-                @if ($counter == 0)
+                @if ($counter == 0 && count($authors) != 5)
                 <input id="author" class="form-control" type="text" name="author[]" value="{{ $author->name }}"/>
                 <i class="input-group-text material-icons" style="color: #008000; font-size: 18.5px" onclick="addAuthor()">add_box</i>
                 @else
@@ -98,14 +150,14 @@
             <div class="col-md-12 mb-3">
               <label class="form-label">Book Description</label>
               <textarea class="form-control" name="description">{{ $book->description }}</textarea>
-              <p>0/500</p>
+              <p>{{ strlen($book->description) }}/500</p>
             </div>
           </div>
         </div>
         <div class="d-flex justify-content-end">
           <div class="btn-block">
             {{-- <button type="submit" class="btn btn-primary ps-4 pe-4" style="background-color: #008000; border-radius: 50px; border: #008000 1px solid">Add</button> --}}
-            <button type="submit" class="btn btn-primary ps-4 pe-4" href="/" style="background-color: #008000; border-radius: 50px; border: #008000 1px solid">Send</button>
+            <button type="submit" class="btn btn-primary ps-4 pe-4" href="/" style="background-color: #008000; border-radius: 50px; border: #008000 1px solid">Save</button>
           </div>
         </div>
       </form>
