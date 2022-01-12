878a95c26f83c21fb9a3659817ada1daa1f5f7f0
Hennoshin <m.irfan.hilmi@gmail.com>
2022-01-12 09:02:15 +0700

    Add search by email

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -9,6 +9,8 @@ use App\Models\Books;
 use App\Models\Author;
 use DB;
 
+use App\Models\User;
+
 class StaffController extends Controller
 {
     public function viewLandingPage()
@@ -18,12 +20,23 @@ class StaffController extends Controller
         ]);
     }
 
-    public function viewAccountManager()
+    public function viewAccountManager(Request $request)
     {
-        $datas = DB::table('users')->get();
+        $usr = User::all();
+
+        $sc = [];
+
+        $pattern = $request->name;
+        $pattern = "/" . $pattern . "/i";
+        foreach ($usr as $u) {
+            $str = $request->is_email == "on" ? $u->email : $u->name;
+            if (preg_match($pattern, $str)) {
+                array_push($sc, $u);
+            }
+        }
 
         return view('staff.accountManager', [
-            'datas' => $datas,
+            'datas' => $sc,
             'page' => 2
         ]);
     }
@@ -41,69 +54,5 @@ class StaffController extends Controller
     public function viewUploadBook()
     {
         return view('staff.uploadbook');
-    }
-
-    public function saveBookInfo(Request $request, AcademicResources $acadres, Books $book)
-    {
-        $acadres->title = $request->title;
-        $acadres->description = $request->description;
-        $acadres->genre = $request->genre;
-        $acadres->publication_place = $request->input("publish-place");
-        $acadres->publication_date = $request->input("publish-date");
-        $acadres->type = 1;
-        if ($request->file("book-file") != null) {
-            $acadres->file_path = $request->file("book-file")->store("books");
-        }
-
-        $acadres->save();
-
-        $book->publisher = $request->publisher;
-        $book->edition = 5;
-        $book->isbn = $request->isbn;
-        $acadres->details()->save($book);
-    }
-
-    public function submitUploadBook(Request $request)
-    {
-        $acadres = new AcademicResources();
-        $book = new Books();
-
-        $this->saveBookInfo($request, $acadres, $book);
-
-        $authorsname = $request->author;
-        if (!is_array($authorsname)) {
-            $authorsname = [$authorsname];
-        }
-
-        foreach ($authorsname as $name) {
-            if ($name != null) {
-                echo $name;
-                $author = Author::where("name", $name)->first();
-                if ($author == null) {
-                    $author = new Author();
-                    $author->name = $name;
-                    $author->save();
-                }
-                $acadres->authors()->attach($name);
-            }
-        }
-
-        return redirect("/");
-    }
-
-    public function editBook(Request $request, $id)
-    {
-        $book = AcademicResources::where("id", $id)->first();
-
-        return view("staff.editbook", [
-            "book" => $book, "authors" => $book->authors, "bookDetails" => $book->details, "id" => $id
-        ]);
-    }
-
-    public function editBookP(Request $request, $id)
-    {
-        $acadres = AcademicResources::where("id", $id)->first();
-
-        $this->saveBookInfo($request, $acadres, $acadres->details);
     }
 }
